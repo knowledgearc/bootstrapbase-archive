d690e41c586aad5d076d269494d62670eea3b42d
pushpan999 <getpushpan@gmail.com>
2016-04-13 16:24:42 +0545

    Added gulp task to copy dir. Added pacakge.json, gulpfile.json and gulp instruction

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var del = require('del');
+var rm = require( 'gulp-rm' )
+
+gulp.task('clean:templates', function () {
+  return del([
+  // clean everything old files inside templates before copying new changed files
+    'D:/projects/htdocs/projects/jcar/templates/knowledgearchive/**/*'
+      ],
+	  {
+		  force: true
+	  });
+});
+
+// gulp.task( 'clean:templates', function() {
+  // return gulp.src( 'D:/projects/htdocs/projects/jcar/templates/knowledgearchive/**/*', { read: false })
+    // .pipe( rm() )
+// })
+
+
+gulp.task('copy', ['clean:templates'], function () {
+        return gulp.src(['templates/knowledgearchive/**/*'], {
+            base: 'templates'
+        }).pipe(gulp.dest('D:/projects/htdocs/projects/jcar/templates'));
+    });
+
+gulp.task('watch', function(){
+	gulp.watch('templates/knowledgearchive/**/*', ['copy']);
+});
+gulp.task('default', ['copy']);
+

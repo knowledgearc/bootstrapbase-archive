025d1c5ed2c996305b8957423fbd1ca86db8cc05
pushpan999 <getpushpan@gmail.com>
2016-04-21 17:37:12 +0545

    edit for new project env (oda)

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,40 @@ var gulp = require('gulp');
 var del = require('del');
 var rm = require( 'gulp-rm' )
 
+// Settings for Jcar Project
+// gulp.task('clean:templates', function () {
+  // return del([
+  // // clean everything old files inside templates before copying new changed files
+    // 'D:/projects/htdocs/projects/jcar/templates/knowledgearchive/**/*'
+      // ],
+	  // {
+		  // force: true
+	  // });
+// });
+
+// // gulp.task( 'clean:templates', function() {
+  // // return gulp.src( 'D:/projects/htdocs/projects/jcar/templates/knowledgearchive/**/*', { read: false })
+    // // .pipe( rm() )
+// // })
+
+
+// gulp.task('copy', ['clean:templates'], function () {
+        // return gulp.src(['templates/knowledgearchive/**/*'], {
+            // base: 'templates'
+        // }).pipe(gulp.dest('D:/projects/htdocs/projects/jcar/templates'));
+    // });
+
+// gulp.task('watch', function(){
+	// gulp.watch('templates/knowledgearchive/**/*', ['copy']);
+// });
+// gulp.task('default', ['copy']);
+
+
+//Settings for Jcar Project
 gulp.task('clean:templates', function () {
   return del([
   // clean everything old files inside templates before copying new changed files
-    'D:/projects/htdocs/projects/jcar/templates/knowledgearchive/**/*'
+    'D:/projects/htdocs/projects/oda/templates/knowledgearchive/**/*'
       ],
 	  {
 		  force: true
@@ -21,7 +51,7 @@ gulp.task('clean:templates', function () {
 gulp.task('copy', ['clean:templates'], function () {
         return gulp.src(['templates/knowledgearchive/**/*'], {
             base: 'templates'
-        }).pipe(gulp.dest('D:/projects/htdocs/projects/jcar/templates'));
+        }).pipe(gulp.dest('D:/projects/htdocs/projects/oda/templates'));
     });
 
 gulp.task('watch', function(){
